340d4d3cf1faa3a7e5c5d14148e289ceb4de8014
Ethan Chinnasamy <ethan.chinnasamy@standardbank.co.za>
2023-12-05 13:24:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._18_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            book book1 = new book("Harry Potter", "JK Rowling", 400);
+
+            Console.WriteLine(book1.title);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2._18_Constructors
+{
+    class book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public book(string eTitle, string aAuthor, int aPages)
+        {
+            title = eTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
